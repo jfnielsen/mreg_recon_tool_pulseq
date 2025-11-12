051978fb2af55159f4f156211f68f520d87cf198
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2025-11-12 15:01:09 -0500

    On branch main 	modified:   main.m

--- Pulseq_sequence/main.m
+++ Pulseq_sequence/main.m
@@ -2,13 +2,13 @@ if ~exist('mr.Sequence');
     setup;
 end
 
-fn = 'myMREG_FOV';
+fn = 'mreg';
 
 % write the .seq file
-MREG_github_FOV;
+%MREG_github_FOV;
 
 % convert to ceq object
-ceq = seq2ceq([fn '.seq'], 'usesRotationEvents', true);
+ceq = seq2ceq([fn '.seq']); %, 'usesRotationEvents', true);
 
 % Check the ceq object:
 % First define hardware parameters
@@ -17,8 +17,8 @@ psd_grd_wait = 100e-6; % ADC-gradient delay, scanner specific (s)
 b1_max = 0.25;         % Gauss
 g_max = 5;             % Gauss/cm
 slew_max = 20;         % Gauss/cm/ms
-coil = 'xrm';          % MR750. See pge2.getsys()
-sysGE = pge2.getsys(psd_rf_wait, psd_grd_wait, b1_max, g_max, slew_max, coil);
+coil = 'xrm';          % MR750. See pge2.opts()
+sysGE = pge2.opts(psd_rf_wait, psd_grd_wait, b1_max, g_max, slew_max, coil);
 
 % check if 'ceq' is compatible with the parameters in 'sys'
 % checks PNS and b1/gradient limits,
@@ -31,11 +31,11 @@ pars = pge2.check(ceq, sysGE);
 S = pge2.plot(ceq, sysGE, 'timeRange', [0 0.1], 'rotate', true, 'interpolate', true);
 check_grad_acoustics(S.gx.signal/100, 'xrm');   % MR750: 'xrm'; UHP: 'hrmbuhp'
 
-return
-
 % Write ceq object to file.
 % pislquant is the number of ADC events used to set Rx gains in Auto Prescan
 writeceq(ceq, [ fn '.pge'], 'pislquant', 10);
+
+return
 
 % After simulating in WTools/VM or scanning, grab the xml files 
 % and compare with the seq object, e.g.:
